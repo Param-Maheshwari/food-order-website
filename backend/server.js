1da3a17c21d0d9fc0aa0518c00bcf0cac0783aa0
Param-Maheshwari <maheshwariparam2004@gmail.com>
2024-10-14 00:31:06 +0530

    Online Food Order Website

--- backend/server.js
+++ backend/server.js
@@ -23,16 +23,16 @@ if (fs.existsSync(ordersFilePath)) {
 
 // Food items (define your food items here with image properties)
 const foodItems = [
-    { id: 1, name: 'Pizza', price: '$10', prepTime: 15, image: '/images/pizza.png' },
-    { id: 2, name: 'Burger', price: '$8', prepTime: 10, image: '/images/burger.png' },
-    { id: 3, name: 'Pasta', price: '$12', prepTime: 20, image: '/images/pasta.png' },
-    { id: 4, name: 'Salad', price: '$7', prepTime: 5, image: '/images/salad.png' },
-    { id: 5, name: 'Sushi', price: '$15', prepTime: 25, image: '/images/sushi.png' },
-    { id: 6, name: 'Tacos', price: '$9', prepTime: 12, image: '/images/tacos.png' },
-    { id: 7, name: 'Noodles', price: '$20', prepTime: 30, image: '/images/noodles.png' },
-    { id: 8, name: 'Ice Cream', price: '$5', prepTime: 5, image: '/images/icecream.png' },
-    { id: 9, name: 'Fried Rice', price: '$4', prepTime: 10, image: '/images/friedrice.png' },
-    { id: 10, name: 'Sandwich', price: '$3', prepTime: 8, image: '/images/sandwich.png' },
+    { id: 1, name: 'Pizza', price: '250 Rs', prepTime: 15, image: '/images/pizza.png' },
+    { id: 2, name: 'Burger', price: '100 Rs', prepTime: 10, image: '/images/burger.png' },
+    { id: 3, name: 'Pasta', price: '120 Rs', prepTime: 20, image: '/images/pasta.png' },
+    { id: 4, name: 'Salad', price: '50 Rs', prepTime: 5, image: '/images/salad.png' },
+    { id: 5, name: 'Sushi', price: '120 Rs', prepTime: 25, image: '/images/sushi.png' },
+    { id: 6, name: 'Tacos', price: '110 Rs', prepTime: 12, image: '/images/tacos.png' },
+    { id: 7, name: 'Noodles', price: '140 Rs', prepTime: 30, image: '/images/noodles.png' },
+    { id: 8, name: 'Ice Cream', price: '40 Rs', prepTime: 5, image: '/images/icecream.png' },
+    { id: 9, name: 'Fried Rice', price: '90 Rs', prepTime: 10, image: '/images/friedrice.png' },
+    { id: 10, name: 'Sandwich', price: '45 Rs', prepTime: 8, image: '/images/sandwich.png' },
 ];
 
 // API to get food items
